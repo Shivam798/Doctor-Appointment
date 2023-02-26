68411ff004fcdea7972aff333c49e2cedbfccdba
Shivam Baghel <shivambghl2000@gmail.com>
2023-02-26 12:02:06 +0530

    Authorization comleted

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,6 +3,8 @@ const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const  router = express.Router();
 const jwt = require('jsonwebtoken')
+const authMiddleware = require('../middlewares/authMiddleware')
+
 
 router.post("/register", async (req, res) => {
   try {
@@ -54,4 +56,18 @@ router.post("/login", async (req, res) => {
 
   }
 });
+
+router.post("/get-user-info-by-id",authMiddleware,async(req,res) => { 
+  try {
+    const user = await User.findOne({_id:req.body.userId})
+    if(!user){
+      return res.status(200).send({message:"User does not exist",success:false})
+    }else{
+      res.status(200).send({success:true,data:{name:user.name,email:user.email}})
+
+    }
+  } catch (error) {
+      res.status(500).send({message:"Error getting user info",success:false})
+  }
+ })
 module.exports = router;
